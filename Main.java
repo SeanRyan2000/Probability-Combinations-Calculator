769de09f0e20d56bf438f1ad733d956e834fa8f5
SeanRyan2000 <58313764+SeanRyan2000@users.noreply.github.com>
2021-03-20 18:54:28 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,43 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		calculate(20, 7, 0.4);
+	}
+	
+	
+	
+	public static void calculate(int numOfTests, int numberOfSuccesses, double probabilityOfSuccess) {
+		double ans =0;
+		double sum = 0;
+		for(int i = numberOfSuccesses; i < numOfTests; i++) {
+			 sum = (float)  ( (factorial(numOfTests)) / ((factorial(i))*factorial((numOfTests-i))) 
+					* (Math.pow(probabilityOfSuccess,i)) * (Math.pow( (1-probabilityOfSuccess),(numOfTests-i) )  ) );
+			System.out.println("PROBABILITY OF IT OCCURING " + i + " TIMES = " + sum);
+			ans = sum + ans;
+			//System.out.println("PROBABILITY OF IT OCCURING ATLEAST " + i + " TIMES = "  +ans);
+		}
+		sum = (Math.pow(probabilityOfSuccess,numOfTests)) * (Math.pow( (1-probabilityOfSuccess),(numOfTests-numOfTests) )  );
+		System.out.println("PROBABILITY OF IT OCCURING " + numOfTests + " TIMES = " + sum);
+		ans = sum + ans;
+		System.out.println("PROBABILITY OF IT OCCURING ATLEAST " + numOfTests + " TIMES ="  +ans);
+		
+		
+		
+		System.out.println(ans);
+		
+	}
+	
+	
+	public static long factorial(int n) {
+	    if (n <= 2) {
+	        return n;
+	    }
+	    return n * factorial(n - 1);
+	}
+			
+			
+}
+
+
